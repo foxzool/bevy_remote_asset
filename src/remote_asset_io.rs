1bba1a5f6627cf1817ec679751910968174175b3
ZoOL <zhooul@gmail.com>
2023-08-27 12:20:01 +0800

    test loader

--- src/remote_asset_io.rs
+++ src/remote_asset_io.rs
@@ -1,6 +1,8 @@
 use std::path::{Path, PathBuf};
 use bevy::asset::{AssetIo, AssetIoError, BoxedFuture, ChangeWatcher, Metadata};
 
+
+
 pub struct RemoterAssetIo {
     pub(crate) default_io: Box<dyn AssetIo>,
 }
@@ -13,7 +15,54 @@ fn is_http(path: &Path) -> bool {
 
 impl AssetIo for RemoterAssetIo {
     fn load_path<'a>(&'a self, path: &'a Path) -> BoxedFuture<'a, Result<Vec<u8>, AssetIoError>> {
-        todo!()
+        if is_http(path) {
+            let uri = path.to_str().unwrap();
+
+            #[cfg(target_arch = "wasm32")]
+                let fut = Box::pin(async move {
+                use wasm_bindgen::JsCast;
+                use wasm_bindgen_futures::JsFuture;
+                let window = web_sys::window().unwrap();
+                let response = JsFuture::from(window.fetch_with_str(uri))
+                    .await
+                    .map(|r| r.dyn_into::<web_sys::Response>().unwrap())
+                    .map_err(|e| e.dyn_into::<js_sys::TypeError>().unwrap());
+
+                if let Err(err) = &response {
+                    // warn!("Failed to fetch asset {uri}: {err:?}");
+                }
+
+                let response = response.map_err(|_| AssetIoError::NotFound(path.to_path_buf()))?;
+
+                let data = JsFuture::from(response.array_buffer().unwrap())
+                    .await
+                    .unwrap();
+
+                let bytes = js_sys::Uint8Array::new(&data).to_vec();
+
+                Ok(bytes)
+            });
+
+            #[cfg(not(target_arch = "wasm32"))]
+                let fut = Box::pin(async move {
+                // let bytes = async_compat::Compat::new(async {
+                //     reqwest::get(uri).await.map_err(|_| AssetIoError::NotFound(path.to_path_buf()))?
+                //         .bytes()
+                //         .await
+                //         .map_err(|_| AssetIoError::NotFound(path.to_path_buf()))?;
+                // });
+                let bytes = reqwest::get(uri).await.map_err(|_| AssetIoError::NotFound(path.to_path_buf()))?
+                    .bytes()
+                    .await
+                    .map_err(|_| AssetIoError::NotFound(path.to_path_buf()))?.to_vec();
+
+                Ok(bytes)
+            });
+
+            fut
+        } else {
+            self.default_io.load_path(path)
+        }
     }
     fn read_directory(&self, path: &Path) -> anyhow::Result<Box<dyn Iterator<Item=PathBuf>>, AssetIoError> {
         self.default_io.read_directory(path)
